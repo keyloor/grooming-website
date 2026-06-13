3f2c642c2a13868fadbbbc896b61cc1fae83a39b
keyloor <150095023+keyloor@users.noreply.github.com>
2026-06-13 11:20:35 -0600

    configura Cypress y scripts de e2e local

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:5173",
+    specPattern: "cypress/e2e/**/*.cy.ts",
+    supportFile: "cypress/support/e2e.ts",
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    video: false,
   },
 });
